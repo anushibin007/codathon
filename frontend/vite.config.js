9481a9cf987bd9b8a1bf4c18ae3480f3814d76c3
Anu Shibin Joseph Raj <anushibin007@gmail.com>
2025-02-27 20:56:41 +0530

    ✨ Dynamic context path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
+
+const BASE_PATH = process.env.VITE_BASE_PATH || "/codathon/";
+
 export default defineConfig({
 	plugins: [react()],
+	base: BASE_PATH,
 });
